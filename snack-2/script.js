0bd8e71ee4e1659586af143b6b789e0a52cb6051
FraAccaBi <francescahuda@gmail.com>
2022-03-31 14:58:32 +0200

    add second snacks

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,5 +1,15 @@
 /*  
+SNACK2
+
 Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo.
 */
+
+let numero = Number(prompt('inserisci un numero'))
+
+if (numero%2 == 0){
+    console.log(numero);
+} else {
+    console.log(numero + 1);
+}
 
