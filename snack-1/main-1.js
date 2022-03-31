5577eef7842aaee59b8195f32b8d0a3b3c8234fa
FraAccaBi <francescahuda@gmail.com>
2022-03-31 12:44:11 +0200

    add first while snack

---
+++ snack-1/main-1.js
@@ -0,0 +1,16 @@
+/*  
+SNACK 1 
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while.
+
+*/
+let somma = 0;
+let index = 0;
+let numero = 0;
+while (index < 5){
+    numero = Number(prompt('inserisci un numero'));
+    somma += numero;
+    index++
+}
+console.log(somma);
