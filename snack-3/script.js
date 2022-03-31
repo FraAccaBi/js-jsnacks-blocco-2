7e6a4878137e7e1481aabb9522b0b5264c2d7c98
FraAccaBi <francescahuda@gmail.com>
2022-03-31 15:08:32 +0200

    add third snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,13 @@
 Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 */
 
+let nomi = ['Fra', 'Riccardo', 'Alice', 'Alessia', 'Claudio']
+let cognomi = ['Huda', 'Rossi', 'Bianchi','Verdi', 'Neri']
+let nomeCompleto =[]
+let index = 0
+while (index < nomi.length){
+    nomeCompleto.push(`${nomi[index]} ${cognomi[index]}`)
+    console.log(nomeCompleto);
+    index++
+}
+
