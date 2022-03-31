5a0cac840a5174dbb5e3f992e9549e27d011365f
FraAccaBi <francescahuda@gmail.com>
2022-03-31 15:40:20 +0200

    add fourth snack

--- snack-4/script.js
+++ snack-4/script.js
@@ -2,3 +2,22 @@
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 */
 
+let myArray = [];
+//genero con un ciclo for i numeri random da inserire nell'array
+for (let i = 0; i<10; i++){
+    let numeroRandom = Math.ceil(Math.random()*10);
+    myArray.push(numeroRandom);
+    console.log(myArray);
+}
+
+//
+
+let somma = 0;
+for (let index = 0; index < myArray.length; index++){
+    if((index%2) == 1){ 
+        console.log(myArray[index]);
+        somma += myArray[index];
+    }
+}
+
+console.log(`la somma dei numeri in posizione dispari è uguale a ${somma}`);
